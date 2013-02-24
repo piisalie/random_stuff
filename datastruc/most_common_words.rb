80cff1857feff40da8b0bf2a5fc71566751d9008
Paul Dawson <excidere@gmail.com>
2013-02-24 10:02:15 -0600

    fixed miscounts due to capitalization

--- datastruc/most_common_words.rb
+++ datastruc/most_common_words.rb
@@ -1,3 +1,3 @@
 frequencies = Hash.new(0)
-File.open('constitution.txt') { |f| f.read }.split.each { |word| frequencies[word] += 1 }
+File.open('constitution.txt') { |f| f.read }.split.each { |word| frequencies[word.downcase] += 1 }
 frequencies.sort_by { |word, freq| freq }.reverse.take(10).each { |word, freq| puts "#{word}: #{freq}" }
