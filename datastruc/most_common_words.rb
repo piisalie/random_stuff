ebcb0d93e343578b3af5619ec2d09c14b2da12fa
Paul Dawson <excidere@gmail.com>
2013-02-26 15:46:36 -0600

    golf

--- datastruc/most_common_words.rb
+++ datastruc/most_common_words.rb
@@ -1,3 +1,5 @@
-frequencies = Hash.new(0)
-File.open('constitution.txt') { |f| f.read }.split.each { |word| frequencies[word.downcase] += 1 }
-frequencies.sort_by { |word, freq| freq }.reverse.take(10).each { |word, freq| puts "#{word}: #{freq}" }
+#frequencies = Hash.new(0)
+#File.open('constitution.txt') { |f| f.read }.split.each { |word| frequencies[word.downcase] += 1 }
+#frequencies.sort_by { |word, freq| freq }.reverse.take(10).each { |word, freq| puts "#{word}: #{freq}" }
+
+Hash.new(0).tap {|h| File.open('constitution.txt') { |f| f.read }.split.each { |w| h[w.downcase] += 1 }}.sort_by { |w, f| f }.reverse.take(10).each { |w,f| puts "#{w}: #{f}"} 
